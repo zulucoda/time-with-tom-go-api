ba70e4ec8f76a3a884eb8dc63a880120a2b68e37
Muzikayise Flynn Buthelezi (zuluCoda) <muzi@mfbproject.co.za>
2020-08-01 21:25:49 +0200

    adding middleware

--- main.go
+++ main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"strconv"
 	"strings"
+	"time"
 )
 
 type Product struct {
@@ -164,9 +166,22 @@ func productHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func middlewareHandler(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("before handler; middleware start")
+		start := time.Now()
+		handler.ServeHTTP(w, r)
+		fmt.Printf("middleware finished; %s", time.Since(start))
+	})
+}
+
 func main() {
-	http.HandleFunc("/products", productsHandler)
-	http.HandleFunc("/products/", productHandler)
+
+	productListHandler := http.HandlerFunc(productsHandler)
+	productItemHandler := http.HandlerFunc(productHandler)
+
+	http.Handle("/products", middlewareHandler(productListHandler))
+	http.Handle("/products/", middlewareHandler(productItemHandler))
 	// port, ServeMux nil is the default
 	http.ListenAndServe(":5000", nil)
 }
