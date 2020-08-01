5ba3e88d6e7b6c2d952f1080b4d871a569d166b1
Muzikayise Flynn Buthelezi (zuluCoda) <muzi@mfbproject.co.za>
2020-08-01 19:31:04 +0200

    using json with handle func

--- main.go
+++ main.go
@@ -1,25 +1,51 @@
 package main
 
-import "net/http"
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
 
-type fooHandler struct {
-	Message string
+type Product struct {
+	ProductID      int    `json: "productId"`
+	Manufacturer   string `json: "manufacturer"`
+	Sku            string `json: "sku"`
+	Upc            string `json: "upc"`
+	PricePerUnit   string `json: "pricePerUnit"`
+	QuantityOnHand int    `json: "quantityOnHand"`
+	ProductName    string `json: "productName"`
 }
 
-func (f *fooHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte(f.Message))
+var productList []Product
+
+func init() {
+	productJSON := `[
+		{"ProductID":1,"Manufacturer":"Alfa Romeo","Sku":"4567qHJK","Upc":"234567893456","PricePerUnit":"99.99","QuantityOnHand":800,"ProductName":"QUADRIFOGLIO badge"},
+		{"ProductID":2,"Manufacturer":"Alfa Romeo","Sku":"44444AF","Upc":"3132123123321","PricePerUnit":"200.50","QuantityOnHand":400,"ProductName":"QUADRIFOGLIO Keys"},
+		{"ProductID":3,"Manufacturer":"Alfa Romeo","Sku":"33333AF","Upc":"8798798546546","PricePerUnit":"40.44","QuantityOnHand":400,"ProductName":"QUADRIFOGLIO Cups"}
+]`
+
+	err := json.Unmarshal([]byte(productJSON), &productList)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
-// always create a pointer to Request
-func barHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("bar called"))
+func productsHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		productsJson, err := json.Marshal(productList)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(productsJson)
+	}
 }
 
 func main() {
-	http.Handle("/foo", &fooHandler{Message: "foo called"})
-
-	http.HandleFunc("/bar", barHandler)
-
+	http.HandleFunc("/products", productsHandler)
 	// port, ServeMux nil is the default
 	http.ListenAndServe(":5000", nil)
 }
