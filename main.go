769621a450e85d5912dcbc236d57e18ddad80256
Muzikayise Flynn Buthelezi (zuluCoda) <muzi@mfbproject.co.za>
2020-08-01 13:47:39 +0200

    creating a handler using simpler handleFunc method

--- main.go
+++ main.go
@@ -10,8 +10,15 @@ func (f *fooHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte(f.Message))
 }
 
+// always create a pointer to Request
+func barHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("bar called"))
+}
+
 func main() {
 	http.Handle("/foo", &fooHandler{Message: "foo called"})
+
+	http.HandleFunc("/bar", barHandler)
 
 	// port, ServeMux nil is the default
 	http.ListenAndServe(":5000", nil)
