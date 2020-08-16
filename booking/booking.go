6d97a0b5e4452b3371e21fefe08a814e99139f58
Muzikayise Flynn Buthelezi (zuluCoda) <muzi@mfbproject.co.za>
2020-08-16 23:45:44 +0200

    updating booking model to include activity

--- booking/booking.go
+++ booking/booking.go
@@ -6,6 +6,7 @@ type Booking struct {
 	Name      string `json:"name"`
 	Surname   string `json:"surname"`
 	Email     string `json:"email"`
+	Activity  string `json:"activity"`
 	Date      string `json:"date"`
 	StartTime string `json:"startTime"`
 	EndTime   string `json:"endTime"`
