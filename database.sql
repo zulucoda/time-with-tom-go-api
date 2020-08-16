16ea90385cba303244cc7d788ec751073c185e89
Muzikayise Flynn Buthelezi (zuluCoda) <muzi@mfbproject.co.za>
2020-08-16 22:59:18 +0200

    adding database.sql

---
+++ database.sql
@@ -0,0 +1,40 @@
+
+CREATE DATABASE `time_with_tom_db` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+CREATE TABLE `users` (
+  `userId` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `surname` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`userId`,`email`),
+  UNIQUE KEY `email_UNIQUE` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=latin1;
+
+CREATE TABLE `activity` (
+  `activityId` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `amount` decimal(13,4) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `minimumTime` int(11) NOT NULL,
+  `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`activityId`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+
+CREATE TABLE `booking` (
+  `bookingId` int(11) NOT NULL AUTO_INCREMENT,
+  `bookingDate` date NOT NULL,
+  `startTime` datetime NOT NULL,
+  `endTime` datetime NOT NULL,
+  `activityId` int(11) NOT NULL,
+  `userId` int(11) NOT NULL,
+  `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`bookingId`),
+  KEY `fk_booking_activity_idx` (`activityId`),
+  KEY `fk_booking_users_idx` (`userId`),
+  CONSTRAINT `fk_booking_activity` FOREIGN KEY (`activityId`) REFERENCES `activity` (`activityId`),
+  CONSTRAINT `fk_booking_users` FOREIGN KEY (`userId`) REFERENCES `users` (`userId`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
