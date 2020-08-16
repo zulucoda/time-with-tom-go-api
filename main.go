2648873d68a5bb9501654b03e001e0a9b7b822df
Muzikayise Flynn Buthelezi (zuluCoda) <muzi@mfbproject.co.za>
2020-08-16 23:01:33 +0200

    opening up DB connection

--- main.go
+++ main.go
@@ -2,13 +2,15 @@ package main
 
 import (
 	"github.com/zulucoda/time-with-tom-go-api/booking"
+	"github.com/zulucoda/time-with-tom-go-api/database"
 	"net/http"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 const apiBasePath = "/api"
 
 func main() {
-
+    database.SetupDatabase()
 	booking.SetupRoutes(apiBasePath)
 	// port, ServeMux nil is the default
 	http.ListenAndServe(":5000", nil)
